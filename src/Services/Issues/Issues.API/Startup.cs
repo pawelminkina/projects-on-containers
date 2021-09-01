025123dfa3674e2ec239cf21cfc71b00e50f73c9
pavixonpl <pawmin14@gmail.com>
2021-09-01 21:16:26 +0200

    some commit

--- src/Services/Issues/Issues.API/Startup.cs
+++ src/Services/Issues/Issues.API/Startup.cs
@@ -83,7 +83,6 @@ namespace Issues.API
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
