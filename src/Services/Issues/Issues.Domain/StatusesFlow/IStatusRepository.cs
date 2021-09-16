e819d0fbd30ce9f32bbad3e829aa62157744f8b4
pavixonpl <pawmin14@gmail.com>
2021-09-16 23:34:20 +0200

    Comment hotfix

--- src/Services/Issues/Issues.Domain/StatusesFlow/IStatusRepository.cs
+++ src/Services/Issues/Issues.Domain/StatusesFlow/IStatusRepository.cs
@@ -6,7 +6,7 @@ namespace Issues.Domain.StatusesFlow
     public interface IStatusRepository
     {
         Task<Status> AddNewStatusAsync(string name, string organizationId);
-        Task<Status> GetStatusById(string id); //comment hot fix but psycha is sitting
+        Task<Status> GetStatusById(string id); 
         Task<IEnumerable<Status>> GetStatusesForOrganization(string organizationId);
         Task RemoveStatusById(string id);
     }
