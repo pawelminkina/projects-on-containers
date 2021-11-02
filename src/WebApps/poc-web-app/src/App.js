7f95c15f7aacffee24f7f25f1efb2e5e29a768e4
pavixonpl <pawmin14@gmail.com>
2021-11-02 22:14:09 +0100

    Material ui added and themes

--- src/WebApps/poc-web-app/src/App.js
+++ src/WebApps/poc-web-app/src/App.js
@@ -1,15 +1,32 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { RouterConfig } from './navigation/RouterConfig';
+import 'fontsource-roboto';
+import { ThemeProvider, Button, createTheme } from '@material-ui/core';
+import { ThemeSwitch } from './components/ThemeSwitch';
+import { dark, light } from './styles/muiTheme';
 
 function App() {
+  const [darkState, setDarkState] = useState(false);
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+    console.log('theme=', darkState ? 'dark' : 'light');
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <BrowserRouter>
-          <RouterConfig />
-        </BrowserRouter>
+        <ThemeProvider theme={darkState ? dark() : light()}>
+          <ThemeSwitch
+            darkState={darkState}
+            handleThemeChange={handleThemeChange}
+          />
+          <BrowserRouter>
+            <RouterConfig />
+          </BrowserRouter>
+        </ThemeProvider>
       </header>
     </div>
   );
