f8b65ad7509b695bd816637903d5e21aab9c2313
pavixonpl <pawmin14@gmail.com>
2022-02-09 15:02:43 +0100

    Regions added in acceptance tests for user service

--- src/Services/Users/Tests/Users.Tests.Acceptance/Services/OrganizationServiceTests.cs
+++ src/Services/Users/Tests/Users.Tests.Acceptance/Services/OrganizationServiceTests.cs
@@ -27,6 +27,8 @@ namespace Users.Tests.Acceptance.Services
             _grpcClient = new OrganizationService.OrganizationServiceClient(channel);
         }
 
+        #region Get All
+
         [Test]
         public async Task ShouldReturnOrganizations()
         {
@@ -44,14 +46,18 @@ namespace Users.Tests.Acceptance.Services
             #region Local methods
 
             IEnumerable<Organization> GetExpectedOrganizations() =>
-             new []
-             {
-                 new Organization() {Enabled = true, Id = "BaseOrganization1", Name = "Base Organization 1", CreationDate = new DateTimeOffset(new DateTime(2020,12,22), new TimeSpan(0,1,0,0)).ToTimestamp()},
-                 new Organization() {Enabled = true, Id = "BaseOrganization2", Name = "Base Organization 2", CreationDate = new DateTimeOffset(new DateTime(2021,12,22), new TimeSpan(0,1,0,0)).ToTimestamp()}
-             };
+                new[]
+                {
+                    new Organization() {Enabled = true, Id = "BaseOrganization1", Name = "Base Organization 1", CreationDate = new DateTimeOffset(new DateTime(2020,12,22), new TimeSpan(0,1,0,0)).ToTimestamp()},
+                    new Organization() {Enabled = true, Id = "BaseOrganization2", Name = "Base Organization 2", CreationDate = new DateTimeOffset(new DateTime(2021,12,22), new TimeSpan(0,1,0,0)).ToTimestamp()}
+                };
 
             #endregion
         }
+
+        #endregion
+
+        #region Get Single
 
         [Test]
         public async Task ShouldReturnOrganization()
@@ -60,7 +66,7 @@ namespace Users.Tests.Acceptance.Services
             var expected = GetExpectedOrganization();
 
             //WHEN organization is retrieved from server
-            var getRequest = new GetOrganizationRequest() {OrganizationId = expected.Id};
+            var getRequest = new GetOrganizationRequest() { OrganizationId = expected.Id };
             var getResponse = await _grpcClient.GetOrganizationAsync(getRequest);
             var actual = getResponse.Organization;
 
@@ -81,6 +87,10 @@ namespace Users.Tests.Acceptance.Services
             #endregion
         }
 
+        #endregion
+
+        #region Create
+
         [Test]
         public async Task ShouldCreateOrganization()
         {
@@ -88,7 +98,7 @@ namespace Users.Tests.Acceptance.Services
             var expected = GetExpectedOrganization();
 
             //WHEN organization is created
-            var createRequest = new AddOrganizationRequest() {Name = expected.Name};
+            var createRequest = new AddOrganizationRequest() { Name = expected.Name };
             var createResponse = await _grpcClient.AddOrganizationAsync(createRequest);
 
             //AND retrieved from server
@@ -119,6 +129,10 @@ namespace Users.Tests.Acceptance.Services
             #endregion
         }
 
+        #endregion
+
+        #region Delete
+
         [Test]
         public async Task ShouldDeleteOrganization()
         {
@@ -126,7 +140,7 @@ namespace Users.Tests.Acceptance.Services
             var organizationToDelete = "BaseOrganization1";
 
             //WHEN organization is deleted
-            var deleteRequest = new DeleteOrganizationRequest() {OrganizationId = organizationToDelete};
+            var deleteRequest = new DeleteOrganizationRequest() { OrganizationId = organizationToDelete };
             var deleteResponse = await _grpcClient.DeleteOrganizationAsync(deleteRequest);
 
             //AND it's retrieved from server
@@ -136,6 +150,8 @@ namespace Users.Tests.Acceptance.Services
             //THEN check does it exist
             exception.Status.StatusCode.Should().Be(StatusCode.NotFound);
         }
+
+        #endregion
 
     }
 }
--- src/Services/Users/Tests/Users.Tests.Acceptance/Services/UserServiceTests.cs
+++ src/Services/Users/Tests/Users.Tests.Acceptance/Services/UserServiceTests.cs
@@ -26,6 +26,8 @@ namespace Users.Tests.Acceptance.Services
             _grpcClient = new UserService.UserServiceClient(channel);
         }
 
+        #region Get All
+
         [Test]
         public async Task ShouldReturnUsersForOrganization()
         {
@@ -36,7 +38,7 @@ namespace Users.Tests.Acceptance.Services
             var organizationId = "BaseOrganization1";
 
             //WHEN users are retrieved from server
-            var getRequest = new GetUsersForOrganizationRequest() {OrganizationId = organizationId };
+            var getRequest = new GetUsersForOrganizationRequest() { OrganizationId = organizationId };
             var getResponse = await _grpcClient.GetUsersForOrganizationAsync(getRequest);
 
             //THEN check equality of expected and actual items
@@ -54,6 +56,11 @@ namespace Users.Tests.Acceptance.Services
             #endregion
         }
 
+        #endregion
+
+        #region Get Single
+
+
         [Test]
         public async Task ShouldReturnUserById()
         {
@@ -61,7 +68,7 @@ namespace Users.Tests.Acceptance.Services
             var expected = GetExpectedUser();
 
             //WHEN user is retrieved from server by expected username
-            var getRequest = new GetUserByIdRequest() {UserId = expected.Id};
+            var getRequest = new GetUserByIdRequest() { UserId = expected.Id };
             var getResponse = await _grpcClient.GetUserByIdAsync(getRequest);
 
             //THEN check equality of expected and actual item
@@ -70,7 +77,7 @@ namespace Users.Tests.Acceptance.Services
             #region Local methods
 
             User GetExpectedUser() =>
-                    new User() {Id= "BaseUser2", OrganizationId = "BaseOrganization1", Fullname = "Base User2", Username = "baseuser2@poc.com", CreatedDate = new DateTimeOffset(new DateTime(2021,12,22), new TimeSpan(0,1,0,0)).ToTimestamp()};
+                new User() { Id = "BaseUser2", OrganizationId = "BaseOrganization1", Fullname = "Base User2", Username = "baseuser2@poc.com", CreatedDate = new DateTimeOffset(new DateTime(2021, 12, 22), new TimeSpan(0, 1, 0, 0)).ToTimestamp() };
 
             #endregion        
         }
@@ -96,6 +103,10 @@ namespace Users.Tests.Acceptance.Services
             #endregion   
         }
 
+        #endregion
+
+        #region Create
+
         [Test]
         public async Task ShouldCreateUser()
         {
@@ -106,7 +117,7 @@ namespace Users.Tests.Acceptance.Services
             var password = "SomeComplex123!@#Password";
 
             //WHEN user is created
-            var createRequest = new CreateUserRequest() {Email = expected.Username, OrganizationId = expected.OrganizationId, Password = password, Fullname = expected.Fullname};
+            var createRequest = new CreateUserRequest() { Email = expected.Username, OrganizationId = expected.OrganizationId, Password = password, Fullname = expected.Fullname };
             var createResponse = await _grpcClient.CreateUserAsync(createRequest);
 
             //AND when created user is retrieved from server
@@ -127,10 +138,14 @@ namespace Users.Tests.Acceptance.Services
             #region Local methods
 
             User GetExpectedUser() =>
-                new User() { OrganizationId = "BaseOrganization2", Fullname = "Base User3", Username = "baseuser3@poc.com"};
+                new User() { OrganizationId = "BaseOrganization2", Fullname = "Base User3", Username = "baseuser3@poc.com" };
 
             #endregion  
         }
+
+        #endregion
+
+        #region Delete
 
         [Test]
         public async Task ShouldDeleteUser()
@@ -139,16 +154,20 @@ namespace Users.Tests.Acceptance.Services
             var userToDelete = "BaseUser1";
 
             //WHEN user is deleted
-            var deleteRequest = new DeleteUserRequest() {UserId = userToDelete};
+            var deleteRequest = new DeleteUserRequest() { UserId = userToDelete };
             var deleteResponse = await _grpcClient.DeleteUserAsync(deleteRequest);
 
             //AND retrieved from server
-            var getRequest = new GetUserByIdRequest() {UserId = userToDelete};
+            var getRequest = new GetUserByIdRequest() { UserId = userToDelete };
             var exception = Assert.ThrowsAsync<RpcException>(async () => await _grpcClient.GetUserByIdAsync(getRequest));
 
             //THEN check that user was not found
             exception.Status.StatusCode.Should().Be(StatusCode.NotFound);
         }
+
+        #endregion
+
+        #region Change password
 
         [Test]
         public async Task ShouldChangeUserPassword()
@@ -163,7 +182,7 @@ namespace Users.Tests.Acceptance.Services
             var newPassword = "TurboNewPassword123!@#";
 
             //WHEN password is changed
-            var changePasswordRequest = new ChangePasswordRequest() {NewPassword = newPassword, OldPassword = oldPassword, UserId = userToChange};
+            var changePasswordRequest = new ChangePasswordRequest() { NewPassword = newPassword, OldPassword = oldPassword, UserId = userToChange };
             var changePasswordResponse = await _grpcClient.ChangePasswordAsync(changePasswordRequest);
 
             //AND check id with password match request is send to server
@@ -174,6 +193,10 @@ namespace Users.Tests.Acceptance.Services
             checkResponse.PasswordMatches.Should().BeTrue();
         }
 
+        #endregion
+
+        #region Email Availability
+
         [Test]
         public async Task ShouldReturnTrueForCheckEmailAvailability()
         {
@@ -181,7 +204,7 @@ namespace Users.Tests.Acceptance.Services
             var email = "emailwhichdontexist@email.com";
 
             //WHEN email is checked
-            var checkRequest = new CheckEmailAvailabilityRequest() {Email = email};
+            var checkRequest = new CheckEmailAvailabilityRequest() { Email = email };
             var checkResponse = await _grpcClient.CheckEmailAvailabilityAsync(checkRequest);
 
             //THEN check that response was true
@@ -202,6 +225,10 @@ namespace Users.Tests.Acceptance.Services
             checkResponse.IsAvailable.Should().BeFalse();
         }
 
+        #endregion
+
+        #region Id And Password Matches
+
         [Test]
         public async Task ShouldReturnTrueForCheckIdAndPasswordMatches()
         {
@@ -210,7 +237,7 @@ namespace Users.Tests.Acceptance.Services
             var password = "1234";
 
             //WHEN check request is send
-            var checkRequest = new CheckIdAndPasswordMatchesRequest() {Password = password, UserId = id};
+            var checkRequest = new CheckIdAndPasswordMatchesRequest() { Password = password, UserId = id };
             var checkResponse = await _grpcClient.CheckIdAndPasswordMatchesAsync(checkRequest);
 
             //THEN check that response is true
@@ -246,5 +273,7 @@ namespace Users.Tests.Acceptance.Services
             //THEN check that response is false
             checkResponse.PasswordMatches.Should().BeFalse();
         }
+
+        #endregion
     }
 }
